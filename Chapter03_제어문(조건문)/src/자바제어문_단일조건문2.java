c4329f6cf1c0f37329649e0032111a9fe96c5da0
sist <sist@sist-101>
2024-11-21 17:52:04 +0900

    1111

--- Chapter03_제어문(조건문)/src/자바제어문_단일조건문2.java
+++ Chapter03_제어문(조건문)/src/자바제어문_단일조건문2.java
@@ -2,6 +2,8 @@
 //               + , - , * , / 
 // 간단한 계산기 (사칙연산 사용)
 import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 public class 자바제어문_단일조건문2 {
 
 	public static void main(String[] args) {
@@ -15,9 +17,36 @@ public class 자바제어문_단일조건문2 {
         // char opp=op.charAt(0)
         System.out.print("연산자 입력(+(43),-(45),*(42),/(47)):");
 		char op=(char)scan.nextInt();
-		System.out.println(num1);
-		System.out.println(num2);
-		System.out.println(op);
+		//System.out.println(num1);
+		//System.out.println(num2);
+		//System.out.println(op);
+		if(op=='+')
+		{
+			System.out.printf("%d + %d = %d\n",num1,num2,num1+num2);
+		}
+		if(op=='-')
+		{
+			/*JOptionPane.showMessageDialog(null, 
+					num1+"-"+num2+"="+(num1-num2));*/
+			System.out.printf("%d - %d = %d\n",num1,num2,num1-num2);
+		}
+		if(op=='*')
+		{
+			/*JOptionPane.showMessageDialog(null, 
+					num1+"-"+num2+"="+(num1-num2));*/
+			System.out.printf("%d * %d = %d\n",num1,num2,num1*num2);
+		}
+		if(op=='/')
+		{
+			/*JOptionPane.showMessageDialog(null, 
+					num1+"-"+num2+"="+(num1-num2));*/
+			// 중첩 조건문 => 이중 if 
+			if(num2==0)
+				System.out.println("0으로 나눌 수 없다");
+			if(num2!=0)
+			    System.out.printf("%d / %d = %d\n",
+			    		num1,num2,num1/num2);
+		}
         
 	}
 
