7220fadaba07552d5c84dc106849d0d5b4173edd
sist <sist@sist-101>
2024-12-10 12:52:30 +0900

    1111

---
+++ Chapter05_객체지향프로그램_2/src/MovieMain.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+public class MovieMain extends JFrame{
+    MovieMain() {
+		// TODO Auto-generated constructor stub
+    	setSize(640, 400);
+    	setVisible(true);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        new MovieMain();// 생성자 
+	}
+
+}
