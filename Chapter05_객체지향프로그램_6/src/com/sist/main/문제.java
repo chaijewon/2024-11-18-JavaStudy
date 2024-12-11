338d314a4bffc2fec6cfbd632a82b5cfee7f7985
sist <sist@sist-101>
2024-12-11 17:52:41 +0900

    111

---
+++ Chapter05_객체지향프로그램_6/src/com/sist/main/문제.java
@@ -0,0 +1,25 @@
+package com.sist.main;
+/*
+ *   1. 객체 / 클래스 
+ *   건축 
+ *   ---
+ *    설계 ===> 시공 ===> 완성 
+ *    ---      ---      ---
+ *    class    구현      | 객체 / 인스턴스 
+ */
+class A
+{
+	A(){
+		
+	}
+	int age;
+}
+public class 문제 {
+    //문제(int s){}
+    //문제(){}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        문제 a=new 문제();
+	}
+
+}
