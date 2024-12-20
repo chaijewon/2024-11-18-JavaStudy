3e5da0e839d6fe8858f28071ef7b7aeef12eea99
sist <sist@sist-101>
2024-12-20 13:53:16 +0900

    1111

---
+++ Chapter10_라이브러리(java.util)/src/com/sist/util/라이브러리_2.java
@@ -0,0 +1,18 @@
+package com.sist.util;
+// StringTokenizer 
+/*
+ *   1. 자른 데이터를 읽기 => nextToken()
+ *   2. 총 자른 갯수 : counterTokens()
+ *   3. 자른 데이터가 있는 동안 : hasMoreTokens()
+ *      => 영화 출연진 => ,
+ *   4. 일반 데이터형 => 사용빈도가 많다 
+ *   5. 네트워크 입출력 
+ */
+public class 라이브러리_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
