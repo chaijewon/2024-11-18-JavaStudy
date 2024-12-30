66c5a1b0ddf117dbc1b7d55583149a09b38dd624
sist <sist@sist-101>
2024-12-30 11:58:02 +0900

    1111

---
+++ 네트워크_프로그램/src/com/sist/client/ClientChatMain.java
@@ -0,0 +1,19 @@
+package com.sist.client;
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
+import com.sist.commons.*;
+public class ClientChatMain extends JFrame{
+    JButton b1,b2;
+    JTextArea ta;
+    JTextField tf;
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+	}
+
+}
